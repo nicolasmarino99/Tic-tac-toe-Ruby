a5f54eefaf7a15cdd48d3424155b7f170952906f
Mauricio Robayo <rfmajo@gmail.com>
2019-08-21 17:47:46 -0500

    Better output for the board

--- lib/game_layout.rb
+++ lib/game_layout.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
+require 'paint'
 
 class GameLayout
-  @board = [1, 2, 3,
-            4, 5, 6,
-            7, 8, 9]
-
-  def print_board(board)
-    puts " #{board[0]} | #{board[1]} | #{board[2]} "
-    puts '-----------'
-    puts " #{board[3]} | #{board[4]} | #{board[5]} "
-    puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  def initialize
+    @board = [1, 2, 3,
+      4, 5, 6,
+      7, 8, 9]
+  end
+  
+  def print_board
+    puts "     ╷   ╷   "
+    puts "   #{Paint['1', :red, :bright]} | #{@board[1]} | #{@board[2]} "
+    puts '  ───┼───┼───'
+    puts "   #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts '  ───┼───┼───'
+    puts "   #{@board[6]} | #{@board[7]} | #{@board[8]} "
+    puts "     ╵   ╵   "
+    puts ""
   end
 end
+
+board = GameLayout.new
+
+board.print_board
