ee5fb62708ac6329b06cd81439a3b57179e516b5
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-08-21 14:31:35 -0500

    added class game-layout feature

--- lib/game_layout.rb
+++ lib/game_layout.rb
@@ -1,15 +1,15 @@
+# frozen_string_literal: true
+
 class GameLayout
-  board = [ 1, 2, 3, 
+  @board = [1, 2, 3,
             4, 5, 6,
-            7, 8, 9 ]
+            7, 8, 9]
 
   def print_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts '-----------'
     puts " #{board[3]} | #{board[4]} | #{board[5]} "
     puts '-----------'
-    puts " #{board[6]} | #{board[7]} | #{board[8]} "  
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
   end
-
-  
 end
