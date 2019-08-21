c7bb1d02d0f718d444bc633230016dd56f6e3d0e
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-08-21 14:53:25 -0500

    defining class game and rules

---
+++ lib/game.rb
@@ -0,0 +1,50 @@
+class Game
+
+  def winner(board)
+    winner = false
+    
+    # test rows
+    3.times do |row|
+      row_array = []
+      3.times do |col|
+        row_array << board[row * 3 + col]
+      end
+      winner = is_winner(row_array)
+    end
+  
+    # test columns
+    3.times do |row|
+      col_array = []
+      3.times do |col|
+        col_array << board[col * 3 + row]
+      end
+      winner = is_winner(col_array)
+    end
+  
+    #test diagonals
+    diagonals = [
+                 [board[0], board[4], board[8]],
+                 [board[2], board[4], board[6]]
+                ]
+    diagonals.each do |diagonal|
+      winner = is_winner(diagonal)
+    end
+  
+    winner 
+  end
+
+  private
+  def is_winner(orientation) 
+    winner = ""
+    if orientation.all? { |cell| cell == 'X' }
+    winner = "X"
+    elsif orientation.all? { |cell| cell == 'O' }
+    winner = "O"
+    end
+    winner
+  end
+
+
+
+
+end
