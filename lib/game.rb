4e366c2f21be3ca6915b9b5022313f0fd052a45a
Mauricio Robayo <rfmajo@gmail.com>
2019-08-22 14:26:11 -0500

    Main logic added

--- lib/game.rb
+++ lib/game.rb
@@ -1,56 +1,24 @@
-# frozen_string_literal: true
+require_relative "./lib/player.rb"
+require_relative "./lib/board.rb"
 
 class Game
-  attr_reader :winner
-  def initialize(player_one_mark = "◯", player_two_mark = "✕")
-    @board = [1, 2, 3,
-              4, 5, 6,
-              7, 8, 9]
-    @winner = ''
-    @player_one_mark = player_two_mark
-    @player_two_mark = player_two_mark
+  def initialize
+    @board = Board.new
+    @player1 = Player.new(@board)
+    @player2 = Player.new(@board)
   end
 
-  def winner?
-    return true if check_array?('rows')
-    return true if check_array?('columns')
+  def play
+    loop do
+      @board.print
 
-    diagonals = [
-      [@board[0], @board[4], @board[8]],
-      [@board[2], @board[4], @board[6]]
-    ]
-    diagonals.each do |diagonal|
-      return true if check_consecutive?(diagonal)
+      @player1.move
+      break if @board.winner? || @board.tie?
+      
+      @board.print
+      
+      @player2.move
+      break if @board.winner? || @board.tie?
     end
-
-    false
-  end
-
-  private
-
-  def check_array?(orientation)
-    3.times do |row|
-      array = []
-      3.times do |col|
-        array << if orientation == 'columns'
-                   @board[col * 3 + row]
-                 else
-                   @board[row * 3 + col]
-                 end
-      end
-      return true if check_consecutive?(array)
-    end
-    false
-  end
-
-  def check_consecutive?(orientation)
-    if orientation.all? { |cell| cell == @player_one_mark }
-      @winner = @player_one_mark
-      return true
-    elsif orientation.all? { |cell| cell == @player_two_mark }
-      @winner = @player_two_mark
-      return true
-    end
-    false
   end
 end
