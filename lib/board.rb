63e0ff607688566ab273eb0dc4911e68236abf9c
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2019-08-22 14:45:46 -0500

    setting the class variables and tie? method in board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,61 @@
 # frozen_string_literal: true
 
+class Board
+  attr_reader :winner
+  def initialize
+    @board = [1,2,3,
+              4,5,6,
+              7,8,9]
+    @winner = ""          
+  end
+
+  def winner?
+    return true if check_array?('rows')
+    return true if check_array?('columns')
+
+    diagonals = [
+      [@board[0], @board[4], @board[8]],
+      [@board[2], @board[4], @board[6]]
+    ]
+    diagonals.each do |diagonal|
+      return true if check_consecutive?(diagonal)
+    end
+
+    false
+  end
+
+  def tie?
+    @board.count("X") == 5       
+  end
+
+
+
+  private
+
+  def check_array?(orientation)
+    3.times do |row|
+      array = []
+      3.times do |col|
+        array << if orientation == 'columns'
+                   @board[col * 3 + row]
+                 else
+                   @board[row * 3 + col]
+                 end
+      end
+      return true if check_consecutive?(array)
+    end
+    false
+  end
+
+  def check_consecutive?(orientation)
+    if orientation.all? { |cell| cell == @player_one_mark }
+      @winner = @player_one_mark
+      return true
+    elsif orientation.all? { |cell| cell == @player_two_mark }
+      @winner = @player_two_mark
+      return true
+    end
+    false
+  end
+
+end
